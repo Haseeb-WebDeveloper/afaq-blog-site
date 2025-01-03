0ada6acfc2ffbfd6dc6820418b488f0b946a3110
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-01-03 21:48:31 +0500

    changing logo

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,11 +11,12 @@ export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 right-0 bg-background/80 backdrop-blur-md z-50 border-b">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-16 overflow-hidden  ">
           <div className="flex items-center">
-            <Link href="/" className="text-2xl font-bold">
+            <Image src="/logo.jpg" alt="Logo" width={100} height={100} className="w-14 h-14" />
+            {/* <Link href="/" className="text-2xl font-bold">
               Blog<span className="text-primary">Space</span>
-            </Link>
+            </Link> */}
           </div>
           
           {/* Desktop Navigation */}
